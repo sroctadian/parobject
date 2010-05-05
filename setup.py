c00c3139c59ed5fa05f0135de3ddb10610c6fb38
sroctadian <syahreza.octadian@gmail.com>
2010-05-05 11:35:47 +0700

    second commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(name='parobjects',
+      version='0.0.1',
+      description='Python objects to handler parameter file.',
+      author='Syahreza Octadian',
+      author_email='syahreza.octadian@gmail.com',
+      url='zhobur.wordpress.com',
+      py_modules=['parameters'],
+      )
+
